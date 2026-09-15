b6dea0d3f24b0e2c006a4da70c6bad26ca69169d
Sahil <sahilsharma9068730737@gmail.com>
2026-09-15 17:49:52 +0530

    x

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
 
-import os
+# import os
 
-print(os.environ.get("gemini_api_key"))
+# print(os.environ.get("gemini_api_key"))
+import sys
+name = sys.stdin.readline()
+sys.stdout.write("Hello "+name)
